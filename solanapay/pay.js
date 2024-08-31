e7bb3244d2be96ceada3ff0bf33f3956fad3e1a6
Elvon <elvonabu22@gmail.com>
2024-08-31 07:35:23 -0800

    express

--- solanapay/pay.js
+++ solanapay/pay.js
@@ -1,6 +1,8 @@
 const {Keypair,Connection,PublicKey,LAMPORTS_PER_SOL,SystemProgram,sendAndConfirmTransaction}=require("@solana/web3.js");
 const {encodeURL,createQR}=require("@solana/pay");
 const bs58 = require('bs58');
+const express=require('express');
+const app= express();
 const { NextApiRequest, NextApiResponse } =require('next');
 require('dotenv').config();
 const myWallet="EgBLkWPZ13jqmY7EyHGrPRxSnQG5LDpcE1EaKfk2qDkn";
@@ -22,23 +24,26 @@ function geturlshi(){
   return qrcode;
 
 }
+app.use(express.json());
+ async function postres(){
 
- async function postres(req,res){
-   try{
-   const accfield=req.body.account;
-   console.log(`this is the the recipients account${accfield}`);
-   const sig=SystemProgram.transfer({
-     fromPubkey:signer.publicKey,
-     toPubkey:"2dLU5sZjVSGn5vBux1qvwnfiPpTpTaJ6YXWQSofGTn3K",
-     lamports:0.1*LAMPORTS_PER_SOL
-   });
-   const tx=new Transaction().add(sig);
-   const txx=await sendAndConfirmTransaction(connection,tx,[signer]);
-   res.status(200).json({message,txx})};
-   catch (error){
-res.status(500).json({error:"did not work as expected"});
+     app.get('/',async(req,res){
+       try{
+       const accfield=req.body.account;
+       console.log(`this is the the recipients account${accfield}`);
+       const sig=SystemProgram.transfer({
+         fromPubkey:signer.publicKey,
+         toPubkey:"2dLU5sZjVSGn5vBux1qvwnfiPpTpTaJ6YXWQSofGTn3K",
+         lamports:0.1*LAMPORTS_PER_SOL
+       });
+       const tx=new Transaction().add(sig);
+       const txx=await sendAndConfirmTransaction(connection,tx,[signer]);
+       res.status(200).json({message,txx})}
+       catch (error){
+      res.status(500).json({error:"did not work as expected"});
+      }
+     })
 
-   }
 }
 
    function main(req,res){
@@ -55,4 +60,4 @@ res.status(500).json({error:"did not work as expected"});
     }
 
 }
-geturlshi();
+main();
